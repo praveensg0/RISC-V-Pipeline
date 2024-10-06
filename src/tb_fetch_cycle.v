6b87940fdeb9343b878d737dc28bd02882bfbfef
Praveen S G <144553645+praveensg0@users.noreply.github.com>
2024-10-06 15:43:48 +0530

    Update tb_fetch_cycle.v

--- src/tb_fetch_cycle.v
+++ src/tb_fetch_cycle.v
@@ -12,7 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-module tb();
+module tb_fetch_cycle();
 
     // Declare I/O
     reg clk =1, rst, PCSrcE;
